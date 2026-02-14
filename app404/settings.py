50674f79e3533f00b89771aa2f152cd0230237cf
ali motaghedi <ali1382motaghedi@gmail.com>
2026-02-14 06:25:06 +0330

    fix: restore settings.py

--- app404/settings.py
+++ app404/settings.py
@@ -15,7 +15,7 @@ DEBUG = env("DEBUG")
 
 ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["localhost", "127.0.0.1"])
 
-TEAM_APPS = [s.strip() for s in env("TEAM_APPS", default="team1,team2,team3,team4,team5,team6,team7,team8,team9").split(",") if s.strip()]
+TEAM_APPS = [s.strip() for s in env("TEAM_APPS", default="team1,team2,team3,team4,team5,team6,team7,team8,team9,team10,team11,team12,team13").split(",") if s.strip()]
 
 INSTALLED_APPS = [
     "django.contrib.admin",
